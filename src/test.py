771f27116088f831f652bdb591507a0a2feccbb7
nicolehmann15 <73998625+nicolehmann15@users.noreply.github.com>
2024-08-28 18:26:38 +0200

    Update test.py
    For the public

--- src/test.py
+++ src/test.py
@@ -10,10 +10,10 @@ from architecture.hyperParameter import get_standard_params, dice_loss, f1_score
 if __name__ == '__main__':
     os.environ['TF_DEVICE_MIN_SYS_MEMORY_IN_MB'] = '100'
     params = get_standard_params()
-    # dataset = Dataset(params['BANDS'], params['num_cls'], params['patch_size'], params['patch_size'], 'D:/Clouds/data/LandSat8/Biome_256_Small_pp/test')
-    # dataset = Dataset(params['BANDS'], params['num_cls'], params['patch_size'], params['patch_size'], 'D:/Clouds/data/Landsat8/Biome_256_Small_pp_md/test_showcase')
-    dataset = Dataset(params['BANDS'], params['num_cls'], params['patch_size'], params['patch_size'], 'D:/Clouds/data/TUBIN/TUBIN_256_pp_md/test')
-    # dataset = Dataset([0], params['num_cls'], params['patch_size'], params['patch_size'], 'D:/Clouds/data/Sentinel-3/Creodias/test')
+    # dataset = Dataset(params['BANDS'], params['num_cls'], params['patch_size'], params['patch_size'], '/Biome_256_Small_pp/test')
+    # dataset = Dataset(params['BANDS'], params['num_cls'], params['patch_size'], params['patch_size'], '/Biome_256_Small_pp_md/test_showcase')
+    dataset = Dataset(params['BANDS'], params['num_cls'], params['patch_size'], params['patch_size'], 'TUBIN_256_pp_md/test')
+    # dataset = Dataset([0], params['num_cls'], params['patch_size'], params['patch_size'], 'Sentinel-3/Creodias/test')
     dataset.create_dataset_np(num_samples=1000)
     num_samples = len(dataset.dataset[0])
     print(str(num_samples) + ' samples in the dataset')
